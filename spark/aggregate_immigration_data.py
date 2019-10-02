3d8704473daa49a3716c4e6c88df86e0daa28d13
Daniel Miller <danielmiller5791@gmail.com>
2019-10-02 09:28:48 +0100

    immigration data filtered

--- spark/aggregate_immigration_data.py
+++ spark/aggregate_immigration_data.py
@@ -9,6 +9,19 @@ CORE_PATH = '/mnt1/data'
 def generate_immigration_rdd(spark, dataset):
   return (spark.read.format("com.github.saurfang.sas.spark").load(dataset))
 
+def return_rdd_column_subset(dataset):
+  return (
+    dataset.select(
+      dataset.i94yr.alias('arrival_year'),
+      dataset.i94mon.alias('arrival_month'),
+      dataset.arrdate.alias('arrival_date'),
+      dataset.depdate.alias('departure_date'),
+      dataset.i94addr.alias('state'),
+      dataset.i94port.alias('city_of_arrival'),
+      dataset.gender,
+      dataset.biryear.alias('age')      
+  ))
+
 def main():
   
   logger = logging.getLogger(__name__)
@@ -26,6 +39,8 @@ def main():
     lambda file: generate_immigration_rdd(spark, file), file_finder.return_file_names()
   )
 
+  immigration_data_sets = map(return_rdd_column_subset, immigration_data_sets)
+  
   example = list(immigration_data_sets)[0]
   logger.error(example.head(100))
 
