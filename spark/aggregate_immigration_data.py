417f43bb6851eedfbf8d7122c0891500a2b4aea9
Daniel Miller <danielmiller5791@gmail.com>
2019-10-06 11:49:39 +0100

    dataframes aggregated into one

--- spark/aggregate_immigration_data.py
+++ spark/aggregate_immigration_data.py
@@ -4,6 +4,11 @@ from lib.file_finder import FileFinder
 from lib.rdd_creator import RDDCreator
 import logging
 import pyspark.sql.functions as f 
+from functools import reduce 
+from pyspark.sql import DataFrame
+
+def unionAll(*dfs):
+    return reduce(DataFrame.union, dfs)
 
 
 CORE_PATH = '/mnt1/data'
@@ -42,6 +47,14 @@ def create_sex_distribution_frame(logger, all_data, female_data):
 
   return distribution_frame
 
+def create_female_distribution(logger, dataset):
+
+    logger.error(dataset.show(10))
+    dataset = dataset.filter(dataset.gender == 'F').groupBy('state', 'arrival_month').count()
+    dataset = dataset.toDF('f_state', 'f_arrival_month', 'f_count')
+    logger.error(dataset.show(10))
+    return dataset
+
 def main():
   
   logger = logging.getLogger(__name__)
@@ -59,35 +72,37 @@ def main():
     lambda file: generate_immigration_rdd(spark, file), file_finder.return_file_names()
   )
 
-  immigration_data_sets = map(return_rdd_column_subset, immigration_data_sets)
-  
-  example = list(immigration_data_sets)[0]
-  example_age_aggregate = example.groupBy('state', 'arrival_month').avg('age')
-  example_sex_aggregate = example.groupBy('state', 'arrival_month').count()
-  
-  example_female_aggregate = example.filter(
-    example.gender == 'F'
-  ).groupBy('state', 'arrival_month').count()
+  immigration_data_sets = list(map(return_rdd_column_subset, immigration_data_sets))
+  immigration_data = unionAll(*immigration_data_sets) 
+  logger.error(immigration_data.show(40))
 
-  sex_distirbution_aggregate = create_sex_distribution_frame(logger, example_sex_aggregate, example_female_aggregate)
 
-  logger.error(example_age_aggregate.show(50))
-  logger.error(example_sex_aggregate.show(50))
-  logger.error(example_female_aggregate.show(50))
-  logger.error(sex_distirbution_aggregate.show(50))
+
+
+
+  # age_aggregate = map(lambda dataset: dataset.groupBy('state', 'arrival_month').avg('age'), immigration_data_sets)
+  # sex_aggregate = map(lambda dataset: dataset.groupBy('state', 'arrival_month').count(), immigration_data_sets)
   
-  example_female_aggregate_filter = example_female_aggregate.filter(
-    example_female_aggregate.state == 'IL'
-  )
+
+  # example = list(immigration_data_sets)[0]
+  # logger.info(example.show(10))
+
+  # female_aggregate = map(
+    # lambda dataset: create_female_distribution(logger, dataset), immigration_data_sets
+  # )
+
+  # example_sex = list(sex_aggregate)[2]
+  # example_all = list(immigration_data_sets)[0]
+  # example_female = list(female_aggregate)[0]
+
+  # logger.error(example_sex.show(10))
+  # logger.error(example_all.show(10))
+  # logger.error(example_female.show(10))
+
+
   
-  all_filter = example_sex_aggregate.filter(
-    example_sex_aggregate.state == 'IL'
-  )
 
-  logger.error(example_female_aggregate_filter.show(50))
-  logger.error(all_filter.show(50))
 
-  logger.error(all_filter.show())
 
 if __name__ == '__main__':
   main()
